2e7b9fe7d7e8a3815095b2c4348da0a1ffda73b4
Vo Van Nam <vovannam050204@gmail.com>
2025-07-21 13:46:50 +0700

    feat: improve UX with beautiful alert

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -3,6 +3,7 @@ import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { useSnackbar } from "notistack";
 
 const CreateBook = () => {
   const [title, setTitle] = useState("");
@@ -10,6 +11,8 @@ const CreateBook = () => {
   const [publishedYear, setPublishedYear] = useState("");
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const { enqueueSnackbar } = useSnackbar();
+
   const handleSaveBook = () => {
     const data = {
       title,
@@ -21,12 +24,14 @@ const CreateBook = () => {
       .post("http://localhost:3000/books", data)
       .then(() => {
         setLoading(false);
+        enqueueSnackbar("Book created successfully!", { variant: "success" });
         navigate("/");
       })
       .catch((error) => {
         setLoading(false);
+        // alert("An error occurred. Please try again later.");
+        enqueueSnackbar("An error occurred. Please try again later.", { variant: "error" });
         console.error(error);
-        alert("An error occurred. Please try again later.");
       });
   };
 
--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -3,6 +3,7 @@ import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
+import { useSnackbar } from "notistack";
 
 const EditBook = () => {
   const [title, setTitle] = useState("");
@@ -11,6 +12,7 @@ const EditBook = () => {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const { id } = useParams();
+  const { enqueueSnackbar } = useSnackbar();
   useEffect(() => {
     setLoading(true);
     axios
@@ -24,7 +26,7 @@ const EditBook = () => {
       })
       .catch((error) => {
         setLoading(false);
-        alert("An error occurred while fetching the book details.");
+        alert("An error occurred. Please try again later.");
         console.error(error);
       });
   }, []);
@@ -39,12 +41,14 @@ const EditBook = () => {
       .put(`http://localhost:3000/books/${id}`, data)
       .then(() => {
         setLoading(false);
+        enqueueSnackbar("Book updated successfully!", { variant: "success" });
         navigate("/");
       })
       .catch((error) => {
         setLoading(false);
+        // alert("An error occurred. Please try again later.");
+        enqueueSnackbar("An error occurred. Please try again later.", { variant: "error" });
         console.error(error);
-        alert("An error occurred. Please try again later.");
       });
   };
 
